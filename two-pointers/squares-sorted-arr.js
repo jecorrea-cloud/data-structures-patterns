63ed3d6a49a3e6e4939906bf0d5a3df35c3f2c3a
joaquin <jecorrea@protonmail.com>
2022-02-20 14:04:36 -0500

    brute force approach

--- two-pointers/squares-sorted-arr.js
+++ two-pointers/squares-sorted-arr.js
@@ -1,1 +1,18 @@
 // Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+// Brute force approach: Generate all the squares and then sort the output array
+
+// O(nlogn) time | O(n) space
+function sortedSquaredArray(array) {
+  // Create a new array for the solution to avoid messing up the input one
+  const sortedSquares = new Array(array.length).fill(0);
+  // Iterate the input array to grab each value
+  for (let index = 0; index < array.length; index++) {
+    const value = array[index];
+    // Index the value to the solution array and then squared it
+    sortedSquares[index] = value * value;
+  }
+  // Sort the soution array and return it
+  sortedSquares.sort((a, b) => a - b);
+  return sortedSquares;
+}
