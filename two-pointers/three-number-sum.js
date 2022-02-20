f46f494cf0e61bd6c09b3f7b8037751809e59206
joaquin <jecorrea@protonmail.com>
2022-02-20 15:55:25 -0500

    barebones solution

--- two-pointers/three-number-sum.js
+++ two-pointers/three-number-sum.js
@@ -2,3 +2,13 @@
 // i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 // Notice that the solution set must not contain duplicate triplets.
+
+function threeNumberSum(array, targetSum) {
+    array.sort((a, b) => a-b)
+    const triplets
+
+    for (let index = 0; index < array.length - 2; index++) {
+        let left = index+1
+        let right = array.length - 1
+    }
+}
