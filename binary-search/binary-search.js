a01a59721b1a0143bc8d27c48d277acb211fc975
joaquin <jecorrea@protonmail.com>
2022-02-27 17:17:49 -0500

    made comment edits. Partial iterative solution added

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -8,19 +8,22 @@
 // Output: 4
 // Explanation: 9 exists in nums and its index is 4
 
-// First approach: Initialize a left and right pointer in the array and calculate a middle pointer based on the index #s.
+// Recursive approach: Initialize a left and right pointer in the array and calculate a middle pointer based on the index #s.
 // Now, if the middle pointer value equals the input target number, we return the index #.
 // Otherwise if the value is greater than it, we 'split' the array and take the values from the left of the
-// middle pointer and repeat the process.
+// middle pointer while moving the right pointer one space from the middle index, and repeat the process.
 // If the value is less than it,  we 'split' the array and take the values from the right of the
-// middle pointer and repeat the process.
-// O(log(n)) time | O(log(n)) space
+// middle pointer while moving the left pointer one space from the middle index, and repeat the process.
+// Because we are eliminating half the values to operate the array every iteration, time complexity will be
+// O(log(n)).
+//
+// O(log(n)) time | O(log(n)) space - where n is the length of the input array
 function binarySearch(array, target) {
   return binarySearchHelper(array, target, 0, array.length - 1);
 }
 
 function binarySearchHelper(array, target, left, right) {
-  // Base case in case the input array is not valid
+  // Base case in case the input target is not valid
   if (left > right) return -1;
   // Declare the middle pointer
   const middle = Math.floor((left + right) / 2);
@@ -29,12 +32,47 @@ function binarySearchHelper(array, target, left, right) {
   // Set a conditional to see if it matches target
   if (potentialMatch === target) {
     return middle;
-  } else if (target < potentialMatch) {
+  }
+  // If it is greater than it, split the array and take the values from the left. Use recursion
+  else if (target < potentialMatch) {
     return binarySearchHelper(array, target, left, middle - 1);
-  } else {
+  }
+  // If it is less than it, split the array and take the values from the right. Use recursion
+  else {
     return binarySearchHelper(array, target, middle + 1, right);
   }
 }
 
-// Space optimal approach:
-// O(log(n)) time | O(1) space
+// Iteratively approach: Unlike the previous approach, an iterator is being implemented.
+// The process will remain the same: We first iterate the array and declare two pointers at the beginning
+// & end of the input array.
+// Next, calculate the middle index and its value. From there we split the array either to the left or right by
+// Just changing the values of the right or left pointers
+// Time complexity will remain to be O(log(n)), whereas space complexity will be constant, given we are not
+// Saving any additional values in memory.
+// O(log(n)) time | O(1) space - where n is the size of the input array
+function binarySearch(array, target) {
+  return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+  // Iterate the array
+  while (left <= right) {
+    // Declare the middle pointer
+    const middle = Math.floor((left + right) / 2);
+    // Declare the value of the middle pointer
+    const potentialMatch = array[middle];
+    // Set a conditional to see if it matches target
+    if (potentialMatch === target) {
+      return middle;
+    }
+    // If it is greater than it, split the array and declare the right pointer from the left. Keep iterating.
+    else if (target < potentialMatch) {
+      right = middle - 1;
+    }
+    // If it is less than it, split the array and take the values from the right. Keep
+    else {
+      left = middle + 1;
+    }
+  }
+}
