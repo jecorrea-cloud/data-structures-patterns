8bc6666e5aebcd6b950194acd05a8e39b4e2883e
joaquin <jecorrea@protonmail.com>
2022-03-17 21:27:28 -0400

    initial prompt

---
+++ arrays/contains_duplicate.js
@@ -0,0 +1,6 @@
+// Given an integer array nums, return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
+
+function containsDuplicate(array) {
+  return false;
+}
--- arrays/spiral_traverse.js
+++ arrays/spiral_traverse.js
@@ -17,3 +17,11 @@
 // Sample Output:
 
 // [1,   2,  3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+// First approach:
+
+function spiralTravers(array) {
+  const result = [];
+
+  return result;
+}
