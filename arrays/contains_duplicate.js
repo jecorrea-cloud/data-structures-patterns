0ad9eab47431b78be197357b73241588baf87729
joaquin <jecorrea@protonmail.com>
2022-04-04 23:10:23 -0400

    brute force approach

--- arrays/contains_duplicate.js
+++ arrays/contains_duplicate.js
@@ -1,6 +1,21 @@
 // Given an integer array nums, return true if any value appears at least twice in the array,
 // and return false if every element is distinct.
 
+// Brute force approach:
+
 function containsDuplicate(array) {
-  return false;
+  let minimumSecondIndex = array.length;
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    for (let j = 0; j < array.length; j++) {
+      const valueToCompare = array[j];
+      if (value === valueToCompare) {
+        minimumSecondIndex = Math.min(minimumSecondIndex, j);
+      }
+    }
+  }
+
+  if (minimumSecondIndex === array.length) return -1;
+
+  return array[minimumSecondIndex];
 }
