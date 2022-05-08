ee6af080b595ee361f41b1f0dd6fe05b62703cbf
joaquin <jecorrea@protonmail.com>
2022-05-08 17:16:58 -0400

    initial prompt

---
+++ basic-algos/numbers-in-a-string.js
@@ -0,0 +1,35 @@
+// Numbers in a string
+
+function countNums(str) {}
+
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 1");
+//   console.log("=>", charCounter("hi", "i"));
+
+//   console.log("");
+
+//   console.log("Expecting: 2");
+//   console.log("=>", charCounter("hello world", "o"));
+
+//   console.log("");
+
+//   console.log("Expecting: 2");
+//   console.log(
+//     "=>",
+//     charCounter("   woah, there are many whitespaces in this case    ", "w")
+//   );
+
+//   console.log("");
+
+//   console.log("Expecting: 5");
+//   console.log(
+//     "=>",
+//     charCounter(
+//       "   yikes, now        whitespaces          in the middle    ",
+//       "e"
+//     )
+//   );
+// }
+
+// module.exports = charCounter;
