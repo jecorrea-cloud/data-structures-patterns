6b42df0935aaf53e722ec3a453ed5f10caa7e5d2
joaquin <jecorrea@protonmail.com>
2022-05-16 11:22:46 -0400

    added comments. Edited function name

--- bitwise-manipulation/single-number.js
+++ bitwise-manipulation/single-number.js
@@ -21,12 +21,16 @@
 // -3 * 10^4 <= nums[i] <= 3 * 10^4
 // Each element in the array appears twice except for one element which appears only once.
 
-// Bitwise approach: Use the bitwise XOR operator. Anything that is compared.
-// Traverse the array once.
+// Bitwise approach: Use the bitwise XOR operator. This operator returns zero if the bits
+// of two compared values are the same, or value n if it is being ccompared to zero.
+// Taking this into consideration, we can iterate over the array and compare each value with
+// a variable that will hold zero initially. Because we are guaranteed there are duplicates
+// and one unique value, these duplicates will cancel each other out regardless of the order
+// they are in the array and eventually yield to the unique value.
 // Time complexity will be linear given the array will be traversed only once.
 // Space complexity is constant because nothing is being saved into memory.
 
-function singleNumbers(nums) {
+function singleNumber(nums) {
   // O(n) time - where n is the length of the input array | O(1) space
   let res = 0;
   for (const value of nums) {
@@ -38,17 +42,17 @@ function singleNumbers(nums) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 1");
-  console.log(singleNumbers([2, 2, 1]));
+  console.log(singleNumber([2, 2, 1]));
 
   console.log("");
 
   console.log("Expecting: 4");
-  console.log("=>", singleNumbers([4, 1, 2, 1, 2]));
+  console.log("=>", singleNumber([4, 1, 2, 1, 2]));
 
   console.log("");
 
   console.log("Expecting: 1");
-  console.log("=>", singleNumbers([1]));
+  console.log("=>", singleNumber([1]));
 }
 
-module.exports = findDuplicate;
+module.exports = singleNumber;
