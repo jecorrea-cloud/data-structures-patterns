d1bc4d7b34ecb015d8d6c8903d8162c3924dc53d
joaquin <jecorrea@protonmail.com>
2022-07-16 17:27:08 -0400

    added first odd approach

--- dynamic-programming/longest-palindromic-substring.js
+++ dynamic-programming/longest-palindromic-substring.js
@@ -14,3 +14,24 @@
 
 // 1 <= s.length <= 1000
 // s consist of only digits and English letters.
+
+function longestPalindrome(s) {
+  let res = "";
+  let resLen = 0;
+
+  for (let index = 0; index < s.length; index++) {
+    // Odd length
+    let l,
+      r = index,
+      index;
+    while ((l > 0) & (r < s.length) & (l[index] === r[index])) {
+      if (r - l + 1 > resLen) {
+        res = s.substring(l, r + 1);
+        resLen = r - l + 1;
+        l -= 1;
+        r += 1;
+      }
+    }
+    // Even length
+  }
+}
