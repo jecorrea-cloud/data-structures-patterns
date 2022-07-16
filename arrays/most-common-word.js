095cbcfad627cd9e1b0d447e3a7b0c3aa5bafe9b
joaquin <jecorrea@protonmail.com>
2022-07-16 18:32:15 -0400

    added hashet approach

--- arrays/most-common-word.js
+++ arrays/most-common-word.js
@@ -24,3 +24,24 @@
 // 0 <= banned.length <= 100
 // 1 <= banned[i].length <= 10
 // banned[i] consists of only lowercase English letters.
+
+function mostCommonWord(paragraph, banned) {
+  const bannedSet = new Set(banned);
+  const words = paragraph.toLowerCase().split(/\W+/);
+  const map = {};
+  let answer = { count: 0, word: "" };
+
+  for (const w of words) {
+    if (!bannedSet.has(w)) {
+      if (map[w] == null) map[w] = 0;
+      map[w]++;
+
+      if (map[w] > answer.count) {
+        answer.count = map[w];
+        answer.word = w;
+      }
+    }
+  }
+
+  return answer.word;
+}
